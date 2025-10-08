7bfd892b96f9f596edd318fe2ee627860230159f
israel escalona flores <israelescalonaflores@gmail.com>
2025-10-08 00:44:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4000485312-9d741",
+  "appId": "1:639053191393:web:039926ff334d6daf03ffa0",
+  "apiKey": "AIzaSyDpi8k5QICIoctqZuY4OJRX4tsjmOPcltA",
+  "authDomain": "studio-4000485312-9d741.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "639053191393"
+};
